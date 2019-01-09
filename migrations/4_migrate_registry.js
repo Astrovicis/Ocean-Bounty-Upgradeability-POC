8f57a5993dde6e117d0640e96b9aa1deb8c85074
Max Howard <max@nanome.ai>
2019-01-09 05:53:02 -0800

    Updated setup

--- migrations/4_migrate_registry.js
+++ migrations/4_migrate_registry.js
@@ -2,7 +2,9 @@ const ContractSystem = artifacts.require('ContractSystem')
 const DIDRegistry = artifacts.require('DIDRegistry')
 const LibDIDRegistry = artifacts.require('LibDIDRegistry')
 
-module.exports = function (deployer) {
-  deployer.deploy(DIDRegistry, ContractSystem.address)
-  deployer.deploy(LibDIDRegistry)
+module.exports = async function (deployer) {
+  await deployer.deploy(DIDRegistry, ContractSystem.address)
+  await deployer.deploy(LibDIDRegistry)
+
+  return LibDIDRegistry.deployed()
 }
